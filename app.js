5767d2212e34df8357553b6f1bc6f3ea99ffdf50
Eric D. Torres <etorresnotary@gmail.com>
2020-09-08 14:56:29 -0500

    creating list of richest people dynamically on html

--- app.js
+++ app.js
@@ -1,3 +1,49 @@
 //drag and drop API
 //https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
 
+//global vars for dom elements
+const draggableListEl = document.querySelector("#draggable-list");
+const checkBtn = document.querySelector("check-btn");
+
+const richPeeps = [
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffet",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zuckerberg",
+  "Michael Bloomberg",
+  "Larry Page",
+];
+
+//store list items
+const listItems = [];
+
+let dragStartIndex;
+
+createList();
+
+//insert list items into HTML and display on DOM
+function createList() {
+
+  [...richPeeps].forEach((person, index) => {
+
+    const listItem = document.createElement("li");
+
+    listItem.setAttribute("data-index", index);
+
+    listItem.innerHTML = `
+    <span class="number">${index + 1}</span>
+    <div class="draggable" draggable="true">
+        <p class="person-name">${person}</p>
+        <i class="fas fa-grip-line"></i>
+    </div>
+    `;
+
+    listItems.push(listItem);
+
+    draggableListEl.appendChild(listItem);
+  });
+}
